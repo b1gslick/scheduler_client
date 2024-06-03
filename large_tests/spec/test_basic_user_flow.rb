c654ca990268a45cf2ccab399aafa392b948ff28
b1gslick <bigslick1106@gmai.com>
2024-06-03 07:41:28 +0300

    perf

--- large_tests/spec/test_basic_user_flow.rb
+++ large_tests/spec/test_basic_user_flow.rb
@@ -27,7 +27,7 @@ describe 'Basic user flow' do
     driver.quit
   end
 
-  it 'User can add several note, play and stop timer, then delete note' do
+  it 'User can add several note, play and stop timer, then delete note', skip: true do
     add_note(1, 60)
     add_note(2, 60)
     press_first_timer
@@ -42,7 +42,7 @@ describe 'Basic user flow' do
     not_visible(1) { expect(driver.find_element(:xpath, '//h2[contains(text(), "test1")]')).to eql(true) }
   end
 
-  it 'User can add several note, play until time is, then note mark as finish' do
+  it 'User can add several note, play until time is, then note mark as finish', skip: true do
     add_note(1, 0.025)
     add_note(2, 0.025)
     press_first_timer
@@ -53,7 +53,7 @@ describe 'Basic user flow' do
     try_for(1) { expect(driver.find_element(:id, 'seconds').text).to eql('00') }
   end
 
-  it 'User can add new note, play time and mark it as finish' do
+  it 'User can add new note, play time and mark it as finish', skip: true do
     add_note(1, 5)
     press_first_timer
     try_for(2) { expect(driver.find_element(:id, 'seconds').text).to eql('59') }
@@ -65,5 +65,21 @@ describe 'Basic user flow' do
     not_visible(1) { expect(driver.find_element(:css, 'button[data-testid="timer-pause"]').displayed?).to eql(true) }
     not_visible(1) { expect(driver.find_element(:css, 'button[data-testid="timer-play"]').displayed?).to eql(true) }
     try_for(1) { expect(driver.find_element(:id, 'seconds').text).to eql(sec_before) }
+  end
+
+  it 'User add note check timer, then edit note, compare timer is changed' do
+    add_note(1, 5)
+    press_first_timer
+    press_pause
+    miniutes_before = driver.find_element(:id, 'minutes').text
+    driver.find_element(:css, 'html').click
+    driver.find_element(:css, 'button[data-testid="note-edit-button"]').click
+    edit = driver.find_element(:css, 'input[data-testid="edit-time"]')
+    edit.clear
+    edit.send_keys(miniutes_before.to_i + 4)
+    driver.find_element(:css, 'button[data-testid="save-button"]').click
+    driver.find_element(:css, 'html').click
+    press_first_timer
+    expect(driver.find_element(:id, 'minutes').text).to eql("0#{miniutes_before.to_i + 4}")
   end
 end
