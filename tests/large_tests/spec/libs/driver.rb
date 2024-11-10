400586c089a08ced0b2e5006defcfa8167be0f2f
b1gslick <bigslick1106@gmai.com>
2024-11-10 09:20:44 +0200

    fix

--- tests/large_tests/spec/libs/driver.rb
+++ tests/large_tests/spec/libs/driver.rb
@@ -8,15 +8,15 @@ module Libs
     attr_accessor :driver
 
     def initialize(driver = @driver)
-      @driver = if grid != 'true'
-                  Selenium::WebDriver.for(browser_type, browser_options)
-                else
-                  Selenium::WebDriver.for(:remote, url: grid_url, http_client: http_client,
-                                                   options: browser_options[:options])
-                end
-      @driver.manage.window.resize_to(1920, 1024)
-      @driver.get(site_url)
-      $driver = @driver
+      driver = if grid != 'true'
+                 Selenium::WebDriver.for(browser_type, browser_options)
+               else
+                 Selenium::WebDriver.for(:remote, url: grid_url, http_client: http_client,
+                                                  options: browser_options[:options])
+               end
+      driver.manage.window.resize_to(1920, 1024)
+      driver.get(site_url)
+      $driver = driver
     end
   end
 end
